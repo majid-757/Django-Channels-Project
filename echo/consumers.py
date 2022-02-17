51bd4e1751415fdbcc38902532d33ef1ed2b5efd
majid-757 <majid.asadollahi95@gmail.com>
2022-02-17 23:19:43 +0330

    Customize Consumer

--- echo/consumers.py
+++ echo/consumers.py
@@ -1,4 +1,6 @@
-from channels.generic.websocket import WebsocketConsumer, AsyncWebSocketConsumer, AsyncJsonWebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer, JsonWebsocketConsumer, AsyncJsonWebsocketConsumer
+from channels.consumer import SyncConsumer, AsyncConsumer
+from channels.exceptions import StopConsumer
 from asgiref.sync import async_to_sync
 import json
 
@@ -24,7 +26,7 @@ class EchoConsumer(WebsocketConsumer):
 
 
 
-class ChatConsumer(AsyncWebSocketConsumer):
+class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.user_id = self.scope['url_route']['kwargs']['username']
         self.group_name = f"chat_{self.user_id}"
@@ -67,8 +69,72 @@ class ChatConsumer(AsyncWebSocketConsumer):
 
 
 
+class ChatConsumer2(AsyncConsumer):
+    
+    async def websocket_connect(self, event):
 
- 
+        self.user_id = self.scope['url_route']['kwargs']['username']
+        self.group_name = f"chat_{self.user_id}"
+
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+
+        await self.send({
+            'type': 'websocket.accept'
+
+        })
+
+        self.send({
+            'type': 'websocket.close'
+
+        })
+
+    async def websocket_disconnect(self, event):
+        
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
+
+        raise StopConsumer()
+
+    async def websocket_receive(self, event):
+        
+        text_data = event.get('text', None)
+        bytes_data = event.get('bytes', None)
+
+
+        if text_data:
+            text_data_json = json.loads(text_data)
+            username = text_data_json['receiver']
+            user_group_name = f"chat_{username}"
+
+            await self.channel_layer.group_send(
+                user_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': text_data
+                }
+            )
+
+
+    async def chat_message(self, event):
+        
+        message = event['message']
+
+        await self.send({
+            'type': 'websocket.send',
+            'text': message
+        })
+
+
+
+
+
+
+
 class TestConsumer(AsyncJsonWebsocketConsumer):
 
     async def connect(self):
--- echo/routing.py
+++ echo/routing.py
@@ -7,6 +7,8 @@ from echo import consumers
 websocket_urlpatterns = [
     path('ws/', consumers.EchoConsumer),
     path('ws/chat/<str:username>/', consumers.ChatConsumer),
+    path('ws/chat2/<str:username>/', consumers.ChatConsumer2),
+
 ]
 
 
