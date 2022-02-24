09355405ee5bf8ccc1a8f3c7102cab25a9480d3f
majid-757 <majid.asadollahi95@gmail.com>
2022-02-24 23:47:33 +0330

    Start Group Chat Project - create urls

---
+++ chat/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from chat import views
+
+app_name = 'chat'
+
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
+from chat import views as chat_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('echo.urls')),
+    path('echo/', include('echo.urls')),
+    path('chat/', include('chat.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='chat/login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(template_name='chat/logout.html'), name='logout'),
+    path('accounts/register/', chat_views.register, name='register'),
+
 ]   
