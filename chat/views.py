76f695f937572290e4de81805eea89bf7ead9e9a
majid-757 <majid.asadollahi95@gmail.com>
2022-02-26 23:41:46 +0330

    Start Group Chat Project - create views

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.utils.safestring import mark_safe
+from django.contrib.auth import login, logout, authenticate
+import json
 
-# Create your views here.
+from .models import Member, GroupChat
+from .forms import UserRegisterForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password2')
+
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('chat:index')
+
+            return redirect('login')
+
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'chat/register.html', {'form': form})
+
+
+
+@login_required
+def index(request):
+    current_user = request.user
+    return render(request, 'chat/index.html', {'chats': current_user.member_set.all()})
+
+
+
+
+
+
+
+
+
+
