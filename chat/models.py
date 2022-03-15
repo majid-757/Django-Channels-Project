9b1e85f2bd8fcb3291b29ab7a28ea0268e9df9bb
majid-757 <majid.asadollahi95@gmail.com>
2022-03-15 21:20:46 +0330

    Start Video Chat Project - create models

--- chat/models.py
+++ chat/models.py
@@ -49,3 +49,34 @@ class Message(models.Model):
 
 
 
+status_list = {
+    0: 'Contacting',
+    1: 'Not available',
+    2: 'Accepted',
+    3: 'Rejected',
+    4: 'Busy',
+    5: 'Proccessing',
+    6: 'Ended',
+}
+
+
+class VideoThread(models.Model):
+    caller = models.ForeignKey(User, on_delete=models.CASCADE, related_name='caller_user')
+    callee = models.ForeignKey(User, on_delete=models.CASCADE, related_name='callee_user')
+    status = models.IntegerField(default=0)
+    date_started = models.DateTimeField(default=datetime.now)
+    date_ended = models.DateTimeField(default=datetime.now)
+    date_created = models.DateTimeField(default=datetime.now)
+
+
+    @property
+    def status_name(self):
+        return status_list[self.status]
+
+
+
+    @property
+    def duration(self):
+        return self.date_ended - self.date_created
+
+
